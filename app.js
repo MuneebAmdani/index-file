5aa477ac852e73c9044ad62ccf81b2a07af50d3e
MuneebAmdani <arhammuneeb239@gmail.com>
2024-11-14 12:39:50 +0500

    javacript learning

---
+++ app.js
@@ -0,0 +1,7 @@
+alert("BOOM");
+prompt("Please enter your name");
+console.log('We are learning Javascript');
+console.log(Math.random()*10);
+console.log(Math.floor(Math.random()*10));
+document.getElementById("demo").innerHTML = 7.5 + 7.5;
+document.getElementById("demo-two").innerHTML = "Muneeb Amdani";
